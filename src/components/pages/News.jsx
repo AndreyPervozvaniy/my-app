728dd77c1651493135c4d1020b06e6f062892d12
AndreyPervozvaniy <andreibelokudria@gmail.com>
2023-09-19 16:53:12 +0300

    add news around

--- src/components/pages/News.jsx
+++ src/components/pages/News.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useMemo, useState } from "react";
-import { Box, Flex, Text, Image, Button } from "@chakra-ui/react";
+import { Box, Flex, Text, Image, Button, Fade } from "@chakra-ui/react";
 import Navbar from "../navigation/Navbar";
 import { newscontent } from "../elem/newscontent";
-
+import Waterlogo from "../../assets/img/watericon.png";
+import { useScrollEvent } from "../../hooks";
 function News() {
   const [page, setPage] = useState(1);
-
+  const [isVisibleTopButton] = useScrollEvent();
   const pagination = useMemo(() => {
     const pagesCount = Math.ceil(newscontent.length / 5);
 
@@ -25,11 +26,42 @@ function News() {
   return (
     <>
       <Navbar />{" "}
+      <Fade in={isVisibleTopButton}>
+        <Button
+          className="fa fa-arrow-up"
+          aria-lavel="topButton"
+          zIndex="12"
+          position="fixed"
+          right={{ base: "5px", xl: "10" }}
+          bottom="55px"
+          w={50}
+          h={50}
+          color="white"
+          background="rgb(63, 72, 204)"
+          borderRadius="full"
+          colorScheme="gray"
+          onClick={() => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }}
+        ></Button>
+      </Fade>{" "}
+      <Box
+        position="fixed"
+        opacity="60%"
+        backgroundImage={`url(${Waterlogo})`}
+        backgroundSize="contain"
+        backgroundRepeat="no-repeat"
+        // backgroundPosition="center center"
+        h="100vh"
+        w="100vw"
+        zIndex="-1"
+      />{" "}
       <Flex justifyContent="center" alignItems="center" h="100vh" w="100%">
         <Box w="90%" h="calc(100vh - 70px)" marginTop="70px">
           {ourNews.map((item) => (
             <Flex
               border="1px solid gray"
+              borderRadius="15px"
               p="10px"
               mb="10px"
               flexDirection="column"
@@ -38,6 +70,8 @@ function News() {
             >
               <Image w="60%" src={item.image}></Image>
               <Text
+                backgroundColor="white"
+                opacity="80%"
                 textAlign="center"
                 justifyContent="center"
                 fontSize={{
@@ -52,23 +86,26 @@ function News() {
               </Text>
             </Flex>
           ))}
-          <Flex justify="space-between">
-            {pagination.map((item, index) => (
-              <Box
-                key={index}
-                border="1px solid black"
-                borderRadius="15%"
-                p="10px"
-                mr="5px"
-                cursor="pointer"
-                onClick={() => setPage(item)}
-                _hover={{
-                  bg: "green",
-                }}
-              >
-                {item}
-              </Box>
-            ))}
+          <Flex justifyContent="center" padding="20px">
+            <Flex justify="space-between" w="100%">
+              {pagination.map((item, index) => (
+                <Flex
+                  justifyContent="center"
+                  alignItems="center"
+                  key={index}
+                  border="1px solid black"
+                  borderRadius="50%"
+                  h="40px"
+                  transition="all 0.5s ease"
+                  w="40px"
+                  cursor="pointer"
+                  onClick={() => setPage(item)}
+                  _hover={{ bg: "rgb(63, 72, 204)", transform: "scale(1.2)" }}
+                >
+                  {item}
+                </Flex>
+              ))}
+            </Flex>
           </Flex>
         </Box>
       </Flex>
